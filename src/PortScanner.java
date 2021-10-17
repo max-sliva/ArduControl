5d184e3632799a3f0bcf755db532ce26f900a7c3
max-sliva <persimon@inbox.ru>
2021-10-18 01:40:50 +0500

    first

---
+++ src/PortScanner.java
@@ -0,0 +1,102 @@
+import jssc.SerialPort;
+import jssc.SerialPortException;
+import jssc.SerialPortList;
+
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
+
+public class PortScanner {
+    private HashMap<String, jssc.SerialPort> portArdus = new HashMap<String, SerialPort>();
+    private ArrayList<String> portNames;
+    private HashMap<String, SerialPort> portsHashMap;
+    private String portInfoJSON = "[]";
+
+    public ArrayList<String> getPortNames(){
+        return portNames;
+    }
+
+    public SerialPort getSerialPortByName(String str) {
+       return portsHashMap.get(str);
+    }
+
+    public SerialPort getSerialPortByArdu(String str) {
+        return portArdus.get(str);
+    }
+
+    public void printPortsArray() { //вывод списка портов
+        System.out.println("Ports changed");
+        if (portNames.size() == 0) System.out.println("No arduinos");
+        else portNames.forEach ( it ->
+            System.out.println(it)
+        );
+    }
+
+    public void startUSBscanner(DefaultListModel<String> listModel) {
+        System.out.println("Start thread for scanning ports");
+        portNames = new ArrayList<>(Arrays.asList(SerialPortList.getPortNames())); // получаем список портов
+        portsHashMap = new HashMap<String, SerialPort>();
+        String[] portNames2 = SerialPortList.getPortNames(); // получаем список портов, с ним будем потом сравнивать новый список
+        printPortsArray();
+        portNames.forEach ( it -> {
+            try {
+                setListnerForArdu(it);
+            } catch (SerialPortException e) {
+                e.printStackTrace();
+            }
+//            Thread.sleep(2000)
+        });
+        ThreadForPortsUpdate threadForPortsUpdate = new ThreadForPortsUpdate(portNames, listModel);
+        threadForPortsUpdate.start();
+//todo Дописать с нвоым подходом к автоопределению
+    }
+
+    public void setListnerForArdu(String port) throws SerialPortException {
+        SerialPort tempPort = new SerialPort(port);
+        portsHashMap.put(port, tempPort);
+        System.out.println("Set listner for $port");
+//    if (tempPort.isOpened) tempPort.closePort()
+        tempPort.openPort(); //открываем порт
+        tempPort.setParams(
+                9600,
+                8,
+                1,
+                0
+        ); //задаем параметры порта, 9600 - скорость, такую же нужно задать для Serial.begin в Arduino
+        AtomicBoolean first = new AtomicBoolean(true);
+        AtomicReference<String> str = new AtomicReference<>("");
+        tempPort.addEventListener( event ->{
+            if (event.isRXCHAR()) {
+                try {
+                    String temp = tempPort.readString();
+                    if (temp!= null) {
+                        if (temp.contains("\n")) str.get().concat(temp);
+                        else str.get().concat(temp.trim());
+//                        else str += temp.trim { it < ' ' && it !='\n'}
+                        if (str.get().contains("end devList")) {
+                            System.out.println("str = "+str);
+
+                        }
+                        str.set("");
+                        if (first.get()) {
+                            tempPort.writeString("1");
+                            first.set(false);
+                        }
+
+                    } else System.out.println("received null from "+port);
+                } catch (SerialPortException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        });
+    }
+
+    public String[] getArdus() {
+        return (String[]) portArdus.keySet().toArray();
+    }
+
+}
---
+++ src/ToArduino.java
@@ -0,0 +1,33 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class ToArduino {
+    public static void main(String[] args) {
+        createGUI();
+    }
+
+    private static void createGUI() {
+        JFrame frame = new JFrame("Arduino devices updater");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setWest(frame);
+        setCenter(frame);
+        frame.setSize(400,300);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
+    private static void setCenter(JFrame frame) {
+    }
+
+    private static void setWest(JFrame frame) { //левая панель со списком портов
+        JList listOfPorts = new JList();
+        DefaultListModel<String> listModel = new DefaultListModel<>();
+        listOfPorts.setModel(listModel);
+        PortScanner portScanner = new PortScanner();
+        portScanner.startUSBscanner(listModel);
+//        listOfPorts.
+
+        frame.add(listOfPorts, BorderLayout.WEST);
+    }
+
+}
